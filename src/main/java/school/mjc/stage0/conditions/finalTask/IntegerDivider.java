c6afb3933c3962dccd40287f85d50ca600c04e4a
v-nebe <katya.shauneva@gmail.com>
2023-01-30 18:18:36 +0300

    tasks completed

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if(divider == 0)
+            System.out.println( "division by zero");
         int result = dividend / divider;
         result *= divider;
         if(result == dividend)
