35eb268932d35bd42dd9f7de512bbb490ba3b197
v-nebe <katya.shauneva@gmail.com>
2023-01-30 18:14:58 +0300

    tasks completed

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -8,13 +8,16 @@ public class DaysInMonth {
             switch (month){
                 case 1, 3, 5, 7, 8, 10, 12:
                     System.out.println(31);
+                    break;
                 case 4, 6, 9, 11:
                     System.out.println(30);
+                    break;
                 case 2:
                     if(year % 4 == 0)
                         System.out.println(29);
                     else
                         System.out.println(28);
+                    break;
             }
         }
         else
--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -5,12 +5,16 @@ public class SeasonDeterminer {
         switch (monthNumber){
             case 12, 1, 2:
                 System.out.println("Winter");
+                break;
             case 3, 4, 5:
                 System.out.println("Spring");
+                break;
             case 6, 7, 8:
                 System.out.println("Summer");
+                break;
             case 9, 10, 11:
                 System.out.println("Autumn");
+                break;
             default:
                 System.out.println("Wrong month number");
         }
